e0ce6ca0fd1292866125e203717d93d4d75f4852
hj222hi <hj222hi@student.lnu.se>
2014-11-11 16:32:52 +0100

    lite på 3an

---
+++ labb 3/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = "";
+            Console.WriteLine("Ange hur många numer som ska matas in:");
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+
+            for (int i = 0; numberOfNumbers > i; i++)
+            {
+                Console.WriteLine("Mata in numer {0}", i+1);
+                string holder = Console.ReadLine();
+                numbers += holder+";";
+            }
+            foreach (string item in numbers) { 
+            
+            }
+          
+        }
+    }
+}
