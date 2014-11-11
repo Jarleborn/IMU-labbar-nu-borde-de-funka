03d237788d2d03fce5f9b51b9d31d159e8ec4224
hj222hi <hj222hi@student.lnu.se>
2014-11-11 14:12:46 +0100

    klar ish

---
+++ labb 2/labb 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string numbers;
+            int counteaven = 0;
+            int countuneaven = 0;
+            int countzero = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Stoppa in ett heltal");
+               
+
+                numbers = Console.ReadLine();
+                int.Parse(numbers);
+
+                foreach (int item in numbers)
+                {
+                    switch (item % 3)
+                    {
+                        case 0:
+                            countzero++;
+                            break;
+                        case 1:
+                            countuneaven++;
+                            break;
+                        case 2:
+                            counteaven++;
+                            break;
+                    }
+                }
+
+                Console.WriteLine("Du har mattat in {0} jämnatal, {1} ojämnatal och {2} nollor", counteaven, countuneaven, countzero);
+
+            }
+        }
+    }
+}
