b5404d357ffa50a683b4d2098434308907383329
hj222hi <hj222hi@student.lnu.se>
2014-11-18 16:48:59 +0100

    labb 4 o 5

---
+++ labb 4/labb 4 emi/labb 4 emi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb_4_emi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string palle;
+            Console.WriteLine("Skrive en rad för att kontrolera om det är ett palindrom");
+            palle = Console.ReadLine();
+            string palle2 =  Reverse(palle);
+
+            if (palle == palle2)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Thohej!! {0} är ett Palindrome", palle);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Nä du {0} är inte ett Palindrome",palle);
+                Console.ResetColor();
+            }
+
+        }
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ labb 5/labb 5/labb 5/Fraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb_5
+{
+    class Fraction
+    {
+        public int getNumerator { }
+        public int getDenominator { }
+
+        public bool isNegative { }
+
+
+    }
+}
