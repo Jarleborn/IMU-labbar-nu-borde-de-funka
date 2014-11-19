6df8b17e5e9aad0e0c9a83135d44c41093dd5b36
hj222hi <hj222hi@student.lnu.se>
2014-11-19 16:15:45 +0100

    labb 5

--- labb 5/labb 5/labb 5/Fraction.cs
+++ labb 5/labb 5/labb 5/Fraction.cs
@@ -8,11 +8,81 @@ namespace labb_5
 {
     class Fraction
     {
-        public int getNumerator { }
-        public int getDenominator { }
+        private int _t;
+        private int _n;
 
-        public bool isNegative { }
+        // täljare
+        public int getNumerator { get { return _t; } set { _t = value; } }
 
+        // nämnare
+        public int getDenominator
+        {
+            get { return _n; }
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Feeel");
+                }
+                _n = value;
+            }
+        }
 
+        public Fraction(int a, int b)
+        {
+            getNumerator = a;
+            try
+            {
+                getDenominator = b;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        //public Fraction() {
+        //}
+
+        public bool isNegative(int a, int b)
+        {
+            if (a < 0 || b < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Fraction add(Fraction a, Fraction b)
+        {
+            if (a.getDenominator == b.getDenominator)
+            {
+
+                return new Fraction(b.getNumerator + a.getNumerator, a.getDenominator);
+            }
+            return new Fraction(a.getNumerator * b.getDenominator + b.getNumerator * a.getDenominator, a.getDenominator * b.getDenominator);
+
+        }
+
+        public Fraction multiply(Fraction a, Fraction b)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            return new Fraction(a.getNumerator * b.getNumerator,
+               a.getDenominator * b.getDenominator);
+
+        }
+
+        public bool isEqualTo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}:{1}", getNumerator, getDenominator);
+        }
     }
 }
--- labb 5/labb 5/labb 5/Program.cs
+++ labb 5/labb 5/labb 5/Program.cs
@@ -10,6 +10,28 @@ namespace labb_5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----Multiplicera----------------");
+            // multiplicera 
+            Fraction a = new Fraction(3, 4);
+            Fraction b = new Fraction(1, 3);
+            Console.WriteLine(a.add(b, a));
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("-----Adderar med lika nämnare--------");
+            // adderar med lika nämnare
+            Fraction a2 = new Fraction(1, 5);
+            Fraction b2 = new Fraction(2, 5);
+            Console.WriteLine(a2.add(b2, a2));
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("-----Adderar med lika nämnare-------");
+            // adderar med lika nämnare
+            Fraction a3 = new Fraction(1, 4);
+            Fraction b3 = new Fraction(1, 3);
+            Console.WriteLine(a3.add(b3, a3));
+            Console.WriteLine("-------------------------------------");
         }
     }
 }
